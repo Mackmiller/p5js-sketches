dea9d52b53720a3e7f907a672523edc7711ffc82
Lars Yencken <lars@yencken.org>
2014-08-10 23:04:19 +1000

    Improve circle-click example.

--- 02-circle-click/sketch.js
+++ 02-circle-click/sketch.js
@@ -1,12 +1,10 @@
 function setup() {
-	createCanvas(640, 480)
+	createCanvas(displayWidth, displayHeight)
 }
 
 function draw() {
 	if (mouseIsPressed) {
 		fill(0)
-	} else {
-		fill(255)
+		ellipse(mouseX, mouseY, 40, 40)
 	}
-	ellipse(mouseX, mouseY, 80, 80);
 }
