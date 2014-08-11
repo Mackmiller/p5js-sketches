515659bc2dd8bd1e47ae2e635af8cafa84f8127d
Lars Yencken <lars@yencken.org>
2014-08-11 17:21:22 +1000

    Add a bezier curve example.

---
+++ 08-bezier/README.md
@@ -0,0 +1,3 @@
+# 08-bezier
+
+[Processing Bezier example](https://www.processing.org/examples/bezier.html), pre-ported on the [Processing transition](https://github.com/lmccart/p5.js/wiki/Processing-transition) page.
---
+++ 08-bezier/sketch.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+	createCanvas(displayWidth, displayHeight);
+	stroke(255, 100);
+	background(0);
+	noFill();
+}
+
+function draw() {
+  for (var i = 0; i < 200; i += 20) {
+    bezier(mouseX - (i / 2.0), mouseY - (i / 2.0), 410, 20, 440, 300, 240 - (i / 16.0), 300 + (i / 8.0));
+  }
+}
