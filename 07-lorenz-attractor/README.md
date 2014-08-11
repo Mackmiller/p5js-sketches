7ed54a4b12777519bb9b458ca6516f393a611e74
Lars Yencken <lars@yencken.org>
2014-08-11 13:43:06 +1000

    Add a Lorenz system.

---
+++ 07-lorenz-attractor/README.md
@@ -0,0 +1,5 @@
+# Lorenz attractor
+
+An animated drawing of a [Lorenz system](http://en.wikipedia.org/wiki/Lorenz_system).
+
+The Lorenz system is in 3D, so we need to pick a particular perspective to view. He we look along the y axis, using x and z as coordinates. This is to avoid a bunch of coordinate change math, and because the result is pretty.
---
+++ 07-lorenz-attractor/sketch.js
@@ -0,0 +1,43 @@
+var sigma = 10;
+var rho = 28;
+var beta = 8 / 3;
+var s = 80;
+
+var x, y, z;
+
+function preload() {
+}
+
+function setup() {
+	createCanvas(displayWidth, displayHeight);
+	x = random(3);
+	y = random(3);
+	z = random(3);
+}
+
+var t = 0;
+var dt = 0.01;
+
+function to_x(v) {
+	// NOTE: arbitrary constant here just to center things
+	return displayWidth * (0.45 + x / s);
+}
+
+function to_z(v) {
+	// NOTE: arbitrary constant here just to center things
+	return displayHeight * (5 + z) / s;
+}
+
+function draw() {
+	var dx = rho * (y - x);
+	var dy = x * (rho - z) - y;
+	var dz = x * y - beta * z;
+
+	x += dx * dt;
+	y += dy * dt;
+	z += dz * dt;
+
+	stroke(0);
+	strokeWeight(2);
+	point(to_x(x), to_z(z));
+}
