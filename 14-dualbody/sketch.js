3b4ceba7eb30461f3c5b458dff15284af3413019
Lars Yencken <lars@yencken.org>
2014-08-22 15:14:46 +1000

    Respawn ship on death.

--- 14-dualbody/sketch.js
+++ 14-dualbody/sketch.js
@@ -9,7 +9,11 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   system = new System();
+  setupMoons(system);
+  setupShip(system);
+}
 
+function setupMoons(system) {
 	var moon1 = new Moon(new p5.Vector(width / 2 - 100, height / 2));
  	moon1.velocity = new p5.Vector(0, -2);
 	system.addBody(moon1);
@@ -17,7 +21,9 @@ function setup() {
 	var moon2 = new Moon(new p5.Vector(width / 2 + 100, height / 2));
 	moon2.velocity = new p5.Vector(0, 2);
 	system.addBody(moon2);
+}
 
+function setupShip(system) {
 	var ship = new Ship(
 		new p5.Vector(width / 2, height / 2)
 	);
@@ -27,4 +33,11 @@ function setup() {
 function draw() {
   background(0);
   system.run();
+  maybeRespawnShip();
+}
+
+function maybeRespawnShip() {
+  if (! system.bodies.some(function(x) { return x instanceof Ship; })) {
+    setupShip(system);
+  }
 }
